fdeee5999685c0328a43bc537e7a82b8112e402c
Santhosh01717 <sureshsanthosh017@gmail.com>
2024-08-11 10:05:42 +0530

    JSX

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 
-const parent = React.createElement("div",{id: "parent"},
- 
-               React.createElement("div",{id: "child"}, 
-            [React.createElement("h1",{id: "head1"},"This is h1"), React.createElement("h2",{id: "head2"}, "This is h2")]));
 
-const heading = React.createElement("h1",{id: "heading"},"Hello World From React!");
-
-const div = React.createElement("div",{id: "top"},[parent,heading]);
-
+const NewComponent = () => (
+     <div>This is new Component</div>
+)
+const heading = (
+    <div>
+    <h1>This is React Element</h1>
+    <NewComponent />
+    </div>
+)
+const HeadingComponent = () =>(
+    <div>
+    <h1>This is React Functional Component</h1>
+    {heading}
+    </div>
+    )
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const head1 = React.createElement("h1",{id: "head1"},"This is h1");
-const head2 = React.createElement("h2",{id: "head2"},"This is h2");
-const child1 = React.createElement("div",{id: "child1"},[head1,head2]);
-const child2 = React.createElement("div",{id: "child2"},[head1,head2]);
-const parent1 = React.createElement("div",{id: "parent1"}, [child1,child2]);
-
-root.render(parent1);
+root.render(<HeadingComponent />);
